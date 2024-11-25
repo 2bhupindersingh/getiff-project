4eedf5c2f32ff8edac70bb498d495edf7ecbbbcd
@2bhupindersingh <bhupindersinghmca2@gmail.com>
2024-11-25 14:46:41 +0530

    Home page latest UI changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-import styles from "./page.module.css";
+import styles from './page.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -8,27 +8,76 @@ import verifiedReviews from '../../public/verified-reviews.svg';
 import gracefullEmployeeImg from '../../public/graceful-employee.svg';
 import stayEmpowered from '../../public/stay-empowered-img.svg';
 import raseConcernImg from '../../public/raise-concern.svg';
+import homeBannerleftImg from '../../public/getiff-banner-left-img.svg';
+import homeBannerRightImg from '../../public/getiff-banner-right-img.svg';
 import { RiLinkedinLine } from 'react-icons/ri';
 import logo from '../../public/logo.svg';
+import { Button, Stack } from 'react-bootstrap';
 
 export default function Home() {
   return (
     <>
       <section className={styles.heroContainer}>
         <section className={styles.heroSection}>
-          <div className={styles.heroContent}>
-            <h1 className={styles.heroTitle}>What&apos;s New on Getiff</h1>
-            <p className={styles.heroText}>
-              Discover the innovative ways Getiff is transforming the workplace for employees and
-              employers alike.
-            </p>
-          </div>
+          <Stack direction="horizontal" className="justify-space-between heroSectionContainer">
+            <Stack
+              direction="horizontal"
+              className="justify-content-between align-items-start hide-on-desktop"
+              gap={4}
+            >
+              <div className="w-50">
+                <Image src={homeBannerleftImg} alt="verified reviews" />
+              </div>
+              <div className="w-50">
+                <Image src={homeBannerRightImg} alt="stay empowered" />
+              </div>
+            </Stack>
+
+            <div className="col-lg-2 col-md-12 col-sm-12 col-xs-12 custom-order-1 hide-on-mobile">
+              <Image src={homeBannerleftImg} alt="verified reviews" />
+            </div>
+            <div className="col-lg-8 col-md-12 col-sm-12 col-xs-12 custom-order-3">
+              <div className={styles.heroContent}>
+                <h1 className={styles.heroTitle}>What&apos;s New on Getiff</h1>
+                <p className={styles.heroText}>
+                  Discover the innovative ways Getiff is transforming the workplace for employees
+                  and employers alike.
+                </p>
+                <Stack
+                  direction="horizontal"
+                  gap={3}
+                  className="justify-content-center btn-container"
+                >
+                  <Link href="/register" className="btn btn-primary btn-register-home">
+                    Register Now
+                  </Link>
+                  <Link href="/register" className="btn btn-outlined btn-contact-home">
+                    Contact Us
+                  </Link>
+                </Stack>
+              </div>
+            </div>
+            <div className="col-lg-2 col-md-12 col-sm-12 col-xs-12 custom-order-2 hide-on-mobile">
+              <Image src={homeBannerRightImg} alt="stay empowered" />
+            </div>
+          </Stack>
         </section>
       </section>
 
+      <section className={styles.centeredSection}>
+        <div className={styles.centeredContainer}>
+          <h6 className={styles.centeredSubheading}>Getiff’s Story</h6>
+          <h5 className={styles.centeredHeading}>Get Transparency, Get Success</h5>
+          <p className={styles.centeredParagraph}>
+            Our platform, Getiff, carefully analyzes your unique employee or company needs, creating
+            a tailored solution to help you verify employee histories, understand company cultures,
+            and resolve workplace disputes efficiently.
+          </p>
+        </div>
+      </section>
       <section className={styles.storySection}>
         <div className={styles.flexContainer}>
-          <div className={`${styles.leftColumn} ${styles.orderClass}`}>
+          <div className={`${styles.leftColumn} ${styles.orderClass} ${styles.contentSpacing}`}>
             <h6 className={styles.storySubHeading}>Employee Reviews</h6>
             <h4 className={styles.storyHeading}>
               Verified Reviews, Powered by
@@ -52,7 +101,7 @@ export default function Home() {
                 alt="Verified Reviews Illustration"
                 fill
                 sizes="(max-width: 768px) 100vw ' 50vw"
-                style={{ objectFit: 'cover' }}
+                style={{ objectFit: 'contain' }}
                 priority
               />
             </div>
@@ -69,11 +118,11 @@ export default function Home() {
                 alt="Employee History Illustration"
                 fill
                 sizes="(max-width: 768px) 100vw, 50vw"
-                style={{ objectFit: 'cover' }}
+                style={{ objectFit: 'contain' }}
               />
             </div>
           </div>
-          <div className={styles.rightColumn}>
+          <div className={`${styles.rightColumn} ${styles.contentSpacing}`}>
             <h6 className={styles.storySubHeading}>Employer Insights</h6>
             <h4 className={styles.storyHeading}>
               Graceful Employee History for
@@ -96,7 +145,7 @@ export default function Home() {
 
       <section className={styles.storySection}>
         <div className={styles.flexContainer}>
-          <div className={`${styles.leftColumn} ${styles.orderClass}`}>
+          <div className={`${styles.leftColumn} ${styles.orderClass} ${styles.contentSpacing}`}>
             <h6 className={styles.storySubHeading}>AI-Powered Insights</h6>
             <h4 className={styles.storyHeading}>
               Stay Empowered With <br />
@@ -121,7 +170,7 @@ export default function Home() {
                 alt="AI Insights Illustration"
                 fill
                 sizes="(max-width: 768px) 100vw' 50vw"
-                style={{ objectFit: 'cover' }}
+                style={{ objectFit: 'contain' }}
               />
             </div>
           </div>
@@ -137,11 +186,13 @@ export default function Home() {
                 alt="Raise Concerns Illustration"
                 fill
                 sizes="(max-width: 768px) 100vw, 50vw"
-                style={{ objectFit: 'cover' }}
+                style={{ objectFit: 'contain' }}
               />
             </div>
           </div>
-          <div className={styles.rightColumn}>
+          <div
+            className={`${styles.rightColumn} ${styles.rightColumnPadding} ${styles.contentSpacing}`}
+          >
             <h6 className={styles.storySubHeading}>Resolve concerns</h6>
             <h4 className={styles.storyHeading}>
               Raise Concerns and Get
